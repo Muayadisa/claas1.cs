8fe433cc9f937e479e79a1662db6cc28f58bb7b7
Muayadisa <62279687+Muayadisa@users.noreply.github.com>
2020-03-24 12:17:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length of array: ");
+
+            // length of array: 
+            int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+
+
+            // number to multiply cells. 
+            Console.WriteLine("Enter number to multiply: ");
+            int NumToMul = Convert.ToInt32(Console.ReadLine());
+
+
+            // initialize array: 
+            int[] arr = new int[lengthOfArray];
+
+            Console.WriteLine($"Num of elements: {lengthOfArray}, num to mul:{NumToMul}");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * NumToMul;
+                Console.Write(arr[i] + ", ");
+            }
+        }
+    }
+}
